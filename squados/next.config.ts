dfc7a769c0a41c088ba3d1b41ecec240e18d7138
Rodrigo <rodrigo@liveequipamentos.com.br>
2026-04-20 16:21:22 -0300

    fix: remover turbopack config para build standalone funcionar

--- squados/next.config.ts
+++ squados/next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: "https://lzzdmrlaizfhwqiolmsx.supabase.co",
     NEXT_PUBLIC_SUPABASE_ANON_KEY: "sb_publishable_Zmh4czz-OWM4kTfYPyE5Cw_z83wrcRc",
   },
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
